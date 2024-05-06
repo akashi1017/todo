8d1731efa7f2eab7ef6ebe719d9fe5264ef92b67
akashi1017 <85441346+akashi1017@users.noreply.github.com>
2024-05-06 09:09:47 +0530

    about us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: 'register', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'tasks', component: TaskListComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', redirectTo: '/register', pathMatch: 'full' },
 ];
 
